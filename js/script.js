0260298150d4cf0e645faedf715dd05d275d4a5f
Wendel dos Santos Nunes <wendelnunes9999@gmail.com>
2021-12-29 11:03:19 -0300

    Project update

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.querySelector('.menu-open').addEventListener('click', function () {
+  document.querySelector('.sidebar-close').style.display = 'flex'
+  document.querySelector('.sidebar').style.display = 'none'
+})
+
+document.querySelector('.menu-close').addEventListener('click', function () {
+  document.querySelector('.sidebar-close').style.display = 'none'
+  document.querySelector('.sidebar').style.display = 'block'
+})
